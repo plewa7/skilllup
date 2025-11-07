7d1855abf40a55c1c2e210c5f8531e604f62e78b
plewa7 <dawid.plewa88@gmail.com>
2025-11-07 13:40:48 +0100

    commit11

--- frontend/tests/api.test.ts
+++ frontend/tests/api.test.ts
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8080";
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8080/index.php";
 
 type Note = {
   id: number;
